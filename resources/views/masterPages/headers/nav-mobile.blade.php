90456ce853f49e8c5619021436e8a9f4dd2d05d3
AsipHamdi <asiphamdi13@gmail.com>
2020-06-05 11:19:11 +0700

    update

--- resources/views/masterPages/headers/nav-mobile.blade.php
+++ resources/views/masterPages/headers/nav-mobile.blade.php
@@ -4,7 +4,7 @@
             <li>
                 <a href="#">Headline</a>
                 <ul class="submenu">
-                    @foreach ($header as $i)
+                    @foreach ($sub_headline as $i)
                         <li><a href="{{ route('sub_kategori','sub_kategori='.$i->id) }}">{{ $i->n_sub_kategori }}</a></li>    
                     @endforeach
                 </ul>
