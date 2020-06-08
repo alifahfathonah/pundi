5dcfbd6b16292a4c98c80c349b44627a027fa38e
AsipHamdi <asiphamdi13@gmail.com>
2020-06-08 13:36:29 +0700

    update

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -9,7 +9,7 @@ use App\User;
 class Artikel extends Model
 {
     protected $table    = 'artikel';
-    protected $fillable = ['id', 'judul', 'kategori_id', 'sub_kategori_id', 'penulis_id', 'gambar', 'isi', 'tag', 'artikel_view', 'created_at', 'updated_at', 'created_by', 'updated_by'];
+    protected $fillable = ['id', 'judul', 'kategori_id', 'sub_kategori_id', 'penulis_id', 'gambar', 'isi', 'tag', 'artikel_view', 'status', 'created_at', 'updated_at', 'created_by', 'updated_by'];
 
     // Relation to table kategori
     public function kategori()
