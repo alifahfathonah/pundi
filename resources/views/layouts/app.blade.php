c7628857ddf4a0aa9d2576551fedb523856e7bd5
AsipHamdi <asiphamdi13@gmail.com>
2020-07-10 15:20:36 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- CSS -->
     @yield('css')
+    <!-- MyStyle -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/util.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
@@ -29,10 +30,10 @@
     <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-
+   
     <!-- Icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+
 </head>
 
 <body style="background-color: white">
@@ -44,6 +45,11 @@
     </main>
 </body>
     @yield('script')
+    <!-- CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <!-- Local -->
     <script src="{{ asset('./assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
     <script src="{{ asset('./assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
     <script src="{{ asset('./assets/js/jquery.slicknav.min.js') }}"></script>
--- resources/views/pages/post-artikel.blade.php
+++ resources/views/pages/post-artikel.blade.php
@@ -1,11 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
-<!-- Header -->
-<div>
-    @include('masterPages.headers.header')
-</div>
-
+@include('masterPages.headers.header')
 <section class="blog_area section-padding" style="margin-top: -80px; margin-bottom: -100px">
     <div class="container">
         <div class="row">
@@ -14,27 +9,27 @@
                 <div class="blog_left_sidebar">
                     <!-- Alert Success -->
                     @if (session()->has('success'))
-                        <div class="alert alert-success alert-dismissible fade show text-center bdr-20 col-md-12 container" role="alert">
-                            {{ session('success') }}
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
+                    <div class="alert alert-success alert-dismissible fade show text-center bdr-5 col-md-12 container" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
                     @endif
                     <!-- Alert Errors -->
                     @if (count($errors) > 0)
-                        <div class="alert alert-danger m-t-30">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            <strong>Whoops Error!</strong>&nbsp;
-                            <span>You have {{ $errors->count() }} error</span>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <strong>Whoops Error!</strong>&nbsp;
+                        <span>You have {{ $errors->count() }} error</span>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     <div class="m-t-50">
                         <form action="{{ route('kirim-tulisan.tambah', Auth::user()->id) }}" method="POST" enctype="multipart/form-data">
@@ -44,35 +39,32 @@
                             <!-- Judul -->
                             <div class="m-t-50">
                                 <label for="" class="f-b fs-17">JUDUL ARTIKEL<span class="text-danger ml-1">*</span></label>
-                                <input type="text" class="single-input border bdr-5 col-md-12" name="judul" id="judul"
-                                    required="" oninvalid="this.setCustomValidity('Judul artikel tidak boleh kosong')"
-                                    oninput="setCustomValidity('')" />
+                                <input type="text" class="input single-input-primary border bdr-5 col-md-12" name="judul" id="judul" required="" oninvalid="this.setCustomValidity('Judul artikel tidak boleh kosong')" oninput="setCustomValidity('')" />
                             </div>
                             <!-- Kategori -->
-                            <div class="m-t-25">
+                            <div class="m-t-15">
                                 <label for="" class="f-b fs-17">KATEGORI<span class="text-danger ml-1">*</span></label><br>
                                 <div class="row">
-                                    <select name="kategori_id" id="kategori_id" class="kategori single-input border col-md-5 select">
-                                        <option value="">- Pilih Kategori -</option>
+                                    <select name="kategori_id" id="kategori_id" class="kategori input single-input-primary border select">
+                                        <option value="0">Pilih Kategori</option>
                                         @foreach ($kategori as $i)
                                             <option value="{{ $i->id }}" @if ($kategori_id == $i->id) selected="selected"@endif>
                                                 {{ $i->n_kategori }}
                                             </option>
                                         @endforeach
                                     </select>
-                                    <select name="sub_kategori_id" id="sub_kategori_id" class="kategori1 single-input border col-md-5 select">
-                                        <option value="">- Pilih Kategori -</option>
+                                    <select name="sub_kategori_id" id="sub_kategori_id" class="kategori1 input single-input-primary border select">
                                     </select>
                                 </div>
                             </div>
                             <!-- Isi -->
-                            <div class="m-t-25">
+                            <div class="m-t-15">
                                 <label for="" class="f-b fs-17">ISI ARTIKEL<span class="text-danger ml-1">*</span></label>
                                 @include('masterPages.summernote')
                             </div>
                             <!-- Gambar -->
                             <div class="alert alert-dismissible" id="message" data-target="#exampleModal" role="alert"></div>
-                            <div class="">
+                            <div style="margin-top: -20px">
                                 <label for="" class="f-b fs-17">GAMBAR UNGGULAN <span class="text-danger ml-1">*</span></label><br>
                                 <input type="file" name="gambar" id="gambar" onchange="tampilkanPreview(this,'preview')">
                                 <label for="file" class="js-labelFile">
@@ -82,11 +74,9 @@
                                 <img width="300" class="rounded img-fluid d-block" id="preview" alt="" style="margin-top: 10px"/>
                             </div>
                             <!-- Tag -->
-                            <div class="m-t-25">
+                            <div class="m-t-10">
                                 <label for="" class="f-b fs-17">TAGS</label>
-                                <input type="text" class="single-input border bdr-5 col-md-12" name="tag" id="tag"
-                                    required="" oninvalid="this.setCustomValidity('Judul artikel tidak boleh kosong')"
-                                    oninput="setCustomValidity('')" />
+                                <input type="text" class="input single-input-primary border bdr-5 col-md-12" name="tag" id="tag"/>
                                 <i class="fs-12 f-red">Pisahkan dengan koma (,)</i>
                             </div>
                             <!-- Button -->
@@ -152,6 +142,7 @@
         }
     }
 
+    // Sub Kategori
     $(document).ready(function(){
         $('#kategori_id').change(function(){ 
             var id=$(this).val();
@@ -164,10 +155,14 @@
                 success: function(data){
                     var html = '';
                     var i;
-                    for(i=0; i<data.length; i++){
-                        html += '<option value='+data[i].id+'>'+data[i].n_sub_kategori+'</option>';
-                    }
-                    $('#sub_kategori_id').html(html);
+                    if (data) {
+                        $.each(data, function(index, value){
+                            html += '<option value='+value.id+'>'+value.n_sub_kategori+'</option>'; 
+                        });
+                        $('#sub_kategori_id').html(html);
+                    } else {
+                        $('#sub_kategori_id').html(html);
+                    }   
                 }
             });
             return false;
