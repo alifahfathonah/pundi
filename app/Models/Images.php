27004ee3f01c4c82db55efde09531720bda26f92
Asip Hamdi <asiphamdi13@gmail.com>
2020-09-01 15:23:26 +0700

    update

--- app/Models/Images.php
+++ app/Models/Images.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Images extends Model
 {
-    protected $table = 'gambar';
+    protected $table    = 'gambar';
     protected $fillable = ['id', 'header', 'footer', 'poster', 'created_at', 'created_by', 'updated_at', 'updated_by'];
 }
